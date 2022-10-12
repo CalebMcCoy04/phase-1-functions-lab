3af4ecbf158a13ea2af678d182409b6b3cf02e63
Caleb McCoy <caleb_mccoy115@yahoo.com>
2022-10-12 16:31:57 -0500

    commit tree 89591f580c99036a6eae7c1a0d5d66818afec1d8

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(location){
+    const hq = 42
+    let numberOfBlocks 
+    if (location > hq){
+      numberOfBlocks = location - hq 
+    } else {
+      numberOfBlocks = hq - location
+    }
+    return  numberOfBlocks
+}
+
+
+function distanceFromHqInFeet(blockNumber){
+  const totalBlocks = distanceFromHqInBlocks(blockNumber)
+  return 264 * totalBlocks
+}
+
+function distanceTravelledInFeet(start, end){
+  let distanceTravelled
+  if (start > end){
+    distanceTravelled = (start - end) * 264
+  } else {
+    distanceTravelled = (end - start) * 264
+  }
+  return distanceTravelled
+}
+
+function calculatesFarePrice(start, destination){
+  const distance = distanceTravelledInFeet(start, destination)
+  if (distance < 400){
+    return 0
+  } else if(distance >= 400 && distance <= 2000){
+    /*
+      so the first 400 feet are free, then charge 2 cents per foot
+    */
+    const notFree = distance - 400
+    return notFree * .02
+  } else if (distance > 2000 && distance < 2500){
+    return 25
+  } else if (distance > 2500){
+    /*
+      it expected 25 to equal 'cannot travel that far'
+    */    
+    return 'cannot travel that far'
+  }
+}
